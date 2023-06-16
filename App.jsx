6cfd38d1b5574f738cba66916b54b1c1181351c8
Kohaya3 <34696344+Kohaya3@users.noreply.github.com>
2023-06-16 16:50:55 -0400

    Add files via upload
    Again, this is my first time using github, so any big mistakes you may notice might be because of that.

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import { Navbar } from './Components/Navbar';
+import Main from './Components/Main';
+import Info from './Info';
+
+export default function App() {
+  return (
+  <>
+    <Navbar />
+    <Main />
+    <Info />
+  </>
+  )
+}
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Info() {
+    return (
+        <div id="info">
+            <h1 id="info-title">Info</h1>
+            <ul id="contact-info">
+                <li>Phone Number: 999-999-9999</li>
+                <li>Email: BlahBlah@gmail.com</li>
+            </ul>
+        </div>
+    );
+}
